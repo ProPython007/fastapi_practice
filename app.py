2a45ae4d6224476ccc3bd8955c713ce7974879a3
ProPython007 <ironheartz367@gmail.com>
2026-03-07 00:53:56 +0530

    match tut 5

--- app.py
+++ app.py
@@ -1,73 +1,120 @@
 # Imports:
-from fastapi import FastAPI, Request, status
-from fastapi.responses import JSONResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.exceptions import HTTPException, RequestValidationError
 from starlette.exceptions import HTTPException as StarletteHTTPException
+from fastapi.exceptions import HTTPException, RequestValidationError
+from fastapi import FastAPI, Request, Depends, status
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import JSONResponse
+from sqlalchemy.orm import Session
+from sqlalchemy import select
 
-from schema import PostCreate, PostResponse
+from typing import Annotated, Any
+
+from schema import PostCreate, PostResponse, UserCreate, UserResponse
+from database import Base, engine, get_db 
+import models
 
 
 # Setup:
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
 app.mount('/static', StaticFiles(directory='static'), name='static')
+app.mount('/media', StaticFiles(directory='media'), name='media')
 
 
-# Rough:
-posts: list[dict] = [
-    {
-        "id": 1,
-        "author": "Corey Schafer",
-        "title": "FastAPI is Awesome",
-        "content": "This framework is really easy to use and super fast.",
-        "date_posted": "April 20, 2025",
-    },
-    {
-        "id": 2,
-        "author": "Jane Doe",
-        "title": "Python is Great for Web Development",
-        "content": "Python is a great language for web development, and FastAPI makes it even better.",
-        "date_posted": "April 21, 2025",
-    },
-]
+# Helper Funcs:
+def check_exists(db: Session, model_obj: Any, val1: Any, val2: Any, return_obj=False):
+    result = db.execute(select(model_obj).where(val1 == val2))
+    if return_obj:
+        return result.scalars().first()
+    return bool(result.scalars().first())
 
 
 # Routes:
 @app.get('/')
 def root():
-    return {'detail': 'fastapi practice'}
+    return {'msg': 'Hello World!'}
 
 
-@app.get("/api/posts", response_model=list[PostResponse])
-def get_posts():
+@app.post(
+    '/api/users',
+    response_model=UserResponse,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_user(user: UserCreate, db: Annotated[Session, Depends(get_db)]):
+    existing_username = check_exists(db, models.User, models.User.username, user.username)
+    if existing_username:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Username already exists!')
+    
+    existing_email = check_exists(db, models.User, models.User.email, user.email)
+    if existing_email:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='User email already exists!')
+    
+    new_user = models.User(
+        username = user.username,
+        email = user.email
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@app.get('/api/users/{user_id}', response_model=UserResponse)
+def get_user(user_id: int, db: Annotated[Session, Depends(get_db)]):
+    user = check_exists(db, models.User, models.User.id, user_id, return_obj=True)
+    if user:
+        return user
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found!')
+
+
+@app.get('/api/users/{user_id}/posts', response_model=list[PostResponse])
+def get_user_posts(user_id: int, db: Annotated[Session, Depends(get_db)]):
+    user = check_exists(db, models.User, models.User.id, user_id)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found!')
+    
+    result = db.execute(select(models.Post).where(models.Post.user_id == user_id))
+    posts = result.scalars().all()
+    return posts
+
+
+@app.get('/api/posts', response_model=list[PostResponse])
+def get_posts(db: Annotated[Session, Depends(get_db)]):
+    result = db.execute(select(models.Post))
+    posts = result.scalars().all()
     return posts
 
 
 @app.post(
-    "/api/posts",
+    '/api/posts',
     response_model=PostResponse,
     status_code=status.HTTP_201_CREATED,
 )
-def create_post(post: PostCreate):
-    new_id = max(p["id"] for p in posts) + 1 if posts else 1
-    new_post = {
-        "id": new_id,
-        "author": post.author,
-        "title": post.title,
-        "content": post.content,
-        "date_posted": "April 23, 2025",
-    }
-    posts.append(new_post)
+def create_post(post: PostCreate, db: Annotated[Session, Depends(get_db)]):
+    user = check_exists(db, models.User, models.User.id, post.user_id)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found!')
+    
+    new_post = models.Post(
+        title = post.title,
+        content = post.content,
+        user_id = post.user_id
+    )
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+
     return new_post
 
 
-@app.get("/api/posts/{post_id}", response_model=PostResponse)
-def get_post(post_id: int):
-    for post in posts:
-        if post.get("id") == post_id:
-            return post
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+@app.get('/api/posts/{post_id}', response_model=PostResponse)
+def get_post(post_id: int, db: Annotated[Session, Depends(get_db)]):
+    post = check_exists(db, models.Post, models.Post.id, post_id, return_obj=True)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Post not found!')
+    return post
 
 
 @app.exception_handler(StarletteHTTPException)
@@ -75,12 +122,12 @@ def general_http_exception_handler(request: Request, exception: StarletteHTTPExc
     message = (
         exception.detail
         if exception.detail
-        else "An error occurred. Please check your request and try again."
+        else 'An error occurred. Please check your request and try again.'
     )
 
     return JSONResponse(
         status_code=exception.status_code,
-        content={"detail": message},
+        content={'detail': message},
     )
 
 
@@ -88,5 +135,5 @@ def general_http_exception_handler(request: Request, exception: StarletteHTTPExc
 def validation_exception_handler(request: Request, exception: RequestValidationError):
     return JSONResponse(
         status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
-        content={"detail": exception.errors()},
+        content={'detail': exception.errors()},
     )
